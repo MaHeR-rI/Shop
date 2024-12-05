451e42c1f5fd312524df28c5daad3e4ae36eb963
Maher Al-Rifaï <maher.refae15@gmail.com>
2024-12-05 23:53:05 +0200

    home cover update

--- src/components/HomeComponents/Cover/Cover.js
+++ src/components/HomeComponents/Cover/Cover.js
@@ -7,23 +7,26 @@ const Cover = () => {
         <div className={classes.coverBody}>
           <div className={classes.coverContent}>
             <h1>FIND CLOTHES THAT MATCHES YOUR STYLE</h1>
-            <p>
+            <p className={classes.subTitle}>
               Browse through our diverse range of meticulously crafted garments,
               designed to bring out your individuality and cater to your sense
               of style.
             </p>
-          </div>
-          <div></div>
-        </div>
-
-        <div className={classes.coverBody}>
-          <div className={classes.coverContent}>
-            <h1>FIND CLOTHES THAT MATCHES YOUR STYLE</h1>
-            <p>
-              Browse through our diverse range of meticulously crafted garments,
-              designed to bring out your individuality and cater to your sense
-              of style.
-            </p>
+            <p className={classes.shopNow}>Shop Now</p>
+            <div className={classes.statistics}>
+              <div>
+                <h2>200+</h2>
+                <span>International Brands</span>
+              </div>
+              <div>
+                <h2>2,000+</h2>
+                <span>High-Quality Products</span>
+              </div>
+              <div>
+                <h2>30,000</h2>
+                <span>Happy Costumers</span>
+              </div>
+            </div>
           </div>
           <div></div>
         </div>
