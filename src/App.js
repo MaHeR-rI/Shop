330691e6b667ddcc94fc1ed18132f7ac07b319d1
Maher Al-Rifaï <maher.refae15@gmail.com>
2024-12-15 20:13:45 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
-import Footer from './components/layout/footerSetion/Footer';
-import Home from './pages/Home';
+import "./App.css";
+import Footer from "./components/layout/footerSetion/Footer";
+import Navbar from "./components/layout/navbarSection/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <Home/>
-    <Footer/>
+      <Navbar />
+      <Home />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/layout/navbarSection/Navbar.js
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import Container from "../../ui/Container";
+import classes from "./Navbar.module.css";
+import brand from "../../../assets/SHOP.CO.png";
+import {
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBIcon,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBBtn,
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem,
+  MDBCollapse,
+} from "mdb-react-ui-kit";
+
+const Navbar = () => {
+  const [openBasic, setOpenBasic] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+  return (
+    <Container>
+      <MDBNavbar expand="lg" className="shadow-0">
+        <MDBContainer fluid className="p-0">
+          <MDBNavbarBrand href="#" className={classes.brand}>
+            <img src={brand} alt="SHOP.CO" />{" "}
+          </MDBNavbarBrand>
+          <MDBNavbarToggler
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+            onClick={() => setOpenBasic(!openBasic)}
+          >
+            <MDBIcon icon="bars" fas />
+          </MDBNavbarToggler>
+
+          <MDBCollapse navbar open={openBasic}>
+            <MDBNavbarNav className="mr-auto mb-2 mb-lg-0 gap-3">
+              <MDBNavbarItem className="text-black ms-3 ">
+                <MDBDropdown>
+                  <MDBDropdownToggle
+                    tag="a"
+                    className="nav-link text-black"
+                    role="button"
+                  >
+                    shop
+                  </MDBDropdownToggle>
+                  <MDBDropdownMenu>
+                    <MDBDropdownItem link>Action</MDBDropdownItem>
+                    <MDBDropdownItem link>Another action</MDBDropdownItem>
+                    <MDBDropdownItem link>Something else here</MDBDropdownItem>
+                  </MDBDropdownMenu>
+                </MDBDropdown>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink active aria-current="page" href="#">
+                  On sale
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href="#" className="text-black">
+                  News Arrivals
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href="#" className="text-black">
+                  Brands
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+            </MDBNavbarNav>
+          </MDBCollapse>
+          <div
+            className={`${classes.form} ${isHovered ? classes.hovered : ""}`}
+          >
+            <form className={`d-flex input-group w-auto`}>
+              <MDBBtn className={classes.search}>
+                <MDBIcon fas icon="search" size='lg'/>
+              </MDBBtn>
+              <input
+                type="text"
+                onFocus={() => {
+                  setIsHovered(true);
+                }}
+                onBlur={() => {
+                  setIsHovered(false);
+                }}
+                className={`${classes.input} form-control`}
+                placeholder="Search for products..."
+                aria-label="Search"
+              />
+            </form>
+          </div>
+          <div className="text-black ms-3 d-flex flex-row gap-3">
+            <a href="/" className="text-black">
+              <MDBIcon size='lg' fas icon="shopping-cart" />
+            </a>
+
+            <a href="/" className="text-black">
+              <MDBIcon size='lg' far icon="user-circle" />
+            </a>
+          </div>
+        </MDBContainer>
+      </MDBNavbar>
+    </Container>
+  );
+};
+export default Navbar;
